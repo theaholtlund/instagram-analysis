eb5bc19bc2aa1181acc33828ac76332caa90ffe2
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-07-24 09:23:41 +0200

    Move out shared configuration variables

---
+++ config.py
@@ -0,0 +1,7 @@
+# Define shared configuration variables
+data_dir = "instagram_data"
+connections_dir = "connections"
+followers_dir = "followers_and_following"
+activity_dir = "your_instagram_activity"
+comments_dir = "comments"
+likes_dir = "likes"
