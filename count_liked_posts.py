5c79c366d31ead4ac6dca9b018a6c91fd0a5067b
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-07-30 09:10:58 +0200

    Update file to only go through HTML file for liked posts

--- count_liked_posts.py
+++ count_liked_posts.py
@@ -19,10 +19,11 @@ def main():
     # Initialise the likes count
     total_likes = 0
     
-    # Iterate over all files in the likes directory
-    for filename in os.listdir(likes_path):
-        file_path = os.path.join(likes_path, filename)
-        
+    # Define the filename to process
+    liked_posts_file = "liked_posts.html"
+    file_path = os.path.join(likes_path, liked_posts_file)
+    
+    if os.path.exists(file_path):
         # Load the HTML content
         likes_html = load_html_content(file_path)
         
@@ -33,17 +34,19 @@ def main():
         likes = extract_likes(likes_soup)
         
         # Update the total likes count
-        total_likes += len(likes)
-    
-    # Define output directory and create it if it does not exist
-    output_dir = create_output_dir(script_dir)
-    
-    # Output the result to a file
-    output_file_path = os.path.join(output_dir, "count_liked_posts.txt")
-    write_to_file_simple(output_file_path, [total_likes], "Total number of likes given on Instagram")
-    
-    # Print out confirmation of file export
-    print(f"The total number of likes given by the user has been saved to '{output_file_path}'.")
+        total_likes = len(likes)
+        
+        # Define output directory and create it if it does not exist
+        output_dir = create_output_dir(script_dir)
+        
+        # Output the result to a file
+        output_file_path = os.path.join(output_dir, "count_liked_posts.txt")
+        write_to_file_simple(output_file_path, [total_likes], "Total number of likes given on Instagram")
+        
+        # Print out confirmation of file export
+        print(f"The total number of likes given by the user has been saved to '{output_file_path}'.")
+    else:
+        print(f"File '{liked_posts_file}' not found in the directory '{likes_path}'.")
 
 if __name__ == "__main__":
     main()
