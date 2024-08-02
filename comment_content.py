0ab9b4cd174d240b0d6a52a21955030fc21c77e9
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-08-02 12:22:30 +0200

    Initial file for new functionality

---
+++ comment_content.py
@@ -0,0 +1,5 @@
+# Import required libraries
+import os
+from bs4 import BeautifulSoup
+import variables
+from utils import load_html_content, write_to_file_detailed, create_output_dir, get_script_dir
