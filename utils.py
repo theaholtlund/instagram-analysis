627078d06e93f1759751b822050bf33cbff2df84
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-08-06 09:18:48 +0200

    Remove redundant functions

--- utils.py
+++ utils.py
@@ -16,14 +16,6 @@ def read_file(file_path, mode):
         print(f"Error: The file '{file_path}' could not be found.")
         return None
 
-# Function to load HTML content with error handling
-def load_html_content(file_path):
-    try:
-        with open(file_path, "r", encoding="utf-8") as file:
-            return file.read()
-    except FileNotFoundError:
-        raise FileNotFoundError(f"Error: '{file_path}' not found. Please make sure you have added the data files to the project correctly.")
-
 # Function to write results to a file in simple format
 def write_to_file_simple(file_path, data, header):
     with open(file_path, "w", encoding="utf-8") as file:
@@ -42,15 +34,6 @@ def write_to_file_detailed(file_path, data, header, data_label):
 # Function to get the directory of the current script
 def get_script_dir():
     return os.path.dirname(os.path.realpath(__file__))
-
-# Function to read lines from a file
-def read_output_file(file_path):
-    try:
-        with open(file_path, "r", encoding="utf-8") as file:
-            return file.readlines()
-    except FileNotFoundError:
-        print(f"Error: The file '{file_path}' could not be found. Please ensure that you have run all the analysis scripts.")
-        return None
 
 # Function to parse a simple output file
 def parse_simple_output(data):
