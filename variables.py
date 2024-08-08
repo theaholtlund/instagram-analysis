486acf93186f899e44b74975847e0f09858d0c9f
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-08-08 10:36:39 +0200

    Add static HTML file names

--- variables.py
+++ variables.py
@@ -1,8 +1,12 @@
-# Define shared configuration variables
+# Define configuration variables for files
+liked_posts_file = "liked_posts.html"
+liked_comments_file = "liked_comments.html"
+
+# Define configuration variables for folders
 data_dir = "instagram_data"
+likes_dir = "likes"
 connections_dir = "connections"
 followers_dir = "followers_and_following"
 activity_dir = "your_instagram_activity"
 comments_dir = "comments"
-likes_dir = "likes"
 output_dir = "analysis_outputs"
