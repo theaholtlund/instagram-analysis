4e1b08c4524a7a82cea7a54913da3ec6aa35d86d
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-08-09 13:20:09 +0200

    Test out implementation of file path constructor

--- not_following_back.py
+++ not_following_back.py
@@ -1,7 +1,7 @@
 # Import required libraries
 import os
 import variables
-from utils import get_script_dir, load_and_parse_html, write_to_file_detailed
+from utils import get_script_dir, construct_file_path, load_and_parse_html, write_to_file_detailed
 
 # Function to extract usernames
 def extract_usernames(soup):
@@ -11,9 +11,12 @@ def extract_usernames(soup):
 def main():
     script_dir = get_script_dir()
     
-    # Define paths for the followers and following HTML files
-    followers_path = os.path.join(script_dir, variables.data_dir, variables.connections_dir, variables.followers_dir, "followers_1.html")
-    following_path = os.path.join(script_dir, variables.data_dir, variables.connections_dir, variables.followers_dir, "following.html")
+    # Define base paths for the followers and following HTML files
+    base_path = construct_file_path(script_dir, variables.data_dir, variables.connections_dir, variables.followers_dir)
+    
+    # Construct paths for the followers and following HTML files
+    followers_path = construct_file_path(base_path, "followers_1.html")
+    following_path = construct_file_path(base_path, "following.html")
     
     # Parse the HTML content using BeautifulSoup
     followers_soup = load_and_parse_html(followers_path)
@@ -26,8 +29,10 @@ def main():
     # Find account names the user is following, that are not following them back
     not_following_back = following_usernames - followers_usernames
     
+    # Construct output file path
+    output_file_path = construct_file_path(variables.output_dir, "not_following_back.txt")
+    
     # Output the result to a file
-    output_file_path = os.path.join(variables.output_dir, "not_following_back.txt")
     write_to_file_detailed(output_file_path, not_following_back, "Accounts not following back", "Usernames not following back")
     
     # Print out confirmation of file export
