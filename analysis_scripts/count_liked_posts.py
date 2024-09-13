600ae1985ad1142120e7cdb6668f302f6c6c39db
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-09-13 16:33:10 +0100

    Update comments

--- analysis_scripts/count_liked_posts.py
+++ analysis_scripts/count_liked_posts.py
@@ -37,7 +37,7 @@ def main():
     # Update the total likes count
     total_likes = len(likes)
     
-    # Output the result to a file
+    # Construct output file path and output the result to file
     output_file_path = construct_file_path(variables.output_dir, "count_liked_posts.txt")
     write_to_file(output_file_path, [total_likes], "Total number of likes given on Instagram: ", detailed=False)
     
