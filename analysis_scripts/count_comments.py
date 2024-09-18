81835ab3230d57e5b0dd479fdf3e068057ed0b6b
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-09-18 09:41:34 +0200

    Update comments

--- analysis_scripts/count_comments.py
+++ analysis_scripts/count_comments.py
@@ -41,7 +41,7 @@ def main():
         # Update the total comment count
         total_comments += len(comments)
     
-    # Output the result to a file
+    # Construct output file path and output the result to file
     output_file_path = construct_file_path(variables.output_dir, "count_comments.txt")
     write_to_file(output_file_path, [total_comments], "Total number of comments left on Instagram: ", detailed=False)
     
