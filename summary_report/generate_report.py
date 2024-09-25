eba4ba8b01b4b8c44a706fbb7794152c7ee3169f
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-09-25 10:50:15 +0200

    Update comments

--- summary_report/generate_report.py
+++ summary_report/generate_report.py
@@ -18,12 +18,13 @@ def generate_html_content(summary_data, show_count_files):
     for file_name, (count, items) in summary_data.items():
         title = capitalise_first_word(file_name.replace("_", " ").replace(".txt", ""))
 
-        # Check if the file is in the list to show the count
+        # Show count in the title if applicable
         if file_name in show_count_files:
             report_content += f"<h2>{title}: {count}</h2>\n"
         else:
             report_content += f"<h2>{title}:</h2>\n"
 
+        # Expandable details for file contents
         if items:
             report_content += "<details><summary>Click to expand</summary>\n"
             report_content += "<div class='content-list'>\n"
@@ -33,14 +34,14 @@ def generate_html_content(summary_data, show_count_files):
     
     return report_content
 
-# Function to generate a summary report from the various analysis output files
+# Function to generate a summary report from analysis output files
 def generate_summary_report():
-    # Define the directories based on the new structure
+    # Define directories and paths
     analysis_output_dir = construct_file_path(root_dir, variables.output_dir)
     summary_file_path = construct_file_path(script_dir, "summary_report.html")
     template_file_path = construct_file_path(script_dir, "report_template.html")
 
-    # Dictionary mapping filenames to their corresponding parser functions
+    # Map filenames to parser functions
     files_parsers = {
         "blocked_accounts.txt": parse_list_output,
         "close_friends.txt": parse_list_output,
@@ -52,7 +53,7 @@ def generate_summary_report():
         "not_following_back.txt": parse_list_output
     }
 
-    # Dictionary to store parsed data from each file
+    # Parse each file and store results
     summary_data = {
         file_name: parser(read_file(construct_file_path(analysis_output_dir, file_name), as_lines=True))
         for file_name, parser in files_parsers.items()
@@ -67,14 +68,14 @@ def generate_summary_report():
     # Read the HTML template
     html_template = read_file(template_file_path)
 
-    # Write to the HTML file with content
+    # Write content into the template and save the report
     with open(summary_file_path, "w", encoding="utf-8") as file:
         file.write(html_template.replace("{{{content}}}", report_content))
 
-    # Print out confirmation of file export and open in browser
+    # Confirmation and automatic report opening
     print(f"The summary report has been generated and saved to '{summary_file_path}'.")
     webbrowser.open(f'file://{os.path.abspath(summary_file_path)}')
 
-# Main function to coordinate execution of the script
+# Main function to trigger the summary report generation
 if __name__ == "__main__":
     generate_summary_report()
