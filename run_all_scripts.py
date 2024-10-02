cc63afb52e713711739881eb3919ab80c62a9f4b
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-10-02 11:08:25 +0200

    Add initial file to run all analysis scripts

---
+++ run_all_scripts.py
@@ -0,0 +1,17 @@
+# Import required libraries
+import os
+import subprocess
+
+# Define the path to the folder where your analysis scripts are located
+scripts_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'analysis_scripts')
+
+# List of scripts to run
+scripts_to_run = [
+    "blocked_accounts.py",
+    "close_friends.py",
+    "content_comments.py",
+    "count_comments.py",
+    "count_liked_comments.py",
+    "count_liked_posts.py",
+    "most_liked_posts.py",
+]
