8d17bcc7491ebda4d67f13c7d6296fb64d53c5dd
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-10-04 09:21:55 +0200

    Add additional variables for analysis files

--- variables.py
+++ variables.py
@@ -3,6 +3,8 @@ BLOCKED_PROFILES_FILE = "blocked_profiles.html"
 CLOSE_FRIENDS_FILE = "close_friends.html"
 LIKED_POSTS_FILE = "liked_posts.html"
 LIKED_COMMENTS_FILE = "liked_comments.html"
+FOLLOWERS_FILE = "followers_1.html"
+FOLLOWING_FILE = "following.html"
 
 # Configuration variables for directories
 DATA_DIR = "instagram_data"
