ae2b3c6e7b6aebdc8e75542db68ac533464a1c5f
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-10-05 10:22:06 +0200

    Use pathlib to define analysis folder

--- run_all_scripts.py
+++ run_all_scripts.py
@@ -1,23 +1,24 @@
 # Import required libraries
 import os
 import subprocess
+from pathlib import Path
 
 # Define path to folder where analysis scripts are located
-scripts_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'analysis_scripts')
+scripts_dir = Path(__file__).resolve().parent / 'analysis_scripts'
 
 # Function to run all scripts in analysis directory
 def run_script(script_name):
     script_path = os.path.join(scripts_dir, script_name)
     try:
-        result = subprocess.run(["python3", script_path], check=True, capture_output=True, text=True)
+        result = subprocess.run(["python3", scripts_dir / script_name], check=True, capture_output=True, text=True)
         print(f"Successfully ran {script_name}:\n{result.stdout}{'=' * 40}\n")
     except subprocess.CalledProcessError as e:
         print(f"There was an error running {script_name}:\n{e.stderr}{'=' * 40}\n")
 
 # Main function to coordinate execution of the script
 def main():
-    # List all files in the analysis script directory
-    scripts_to_run = os.listdir(scripts_dir)
+    # List all Python files in the analysis script directory
+    scripts_to_run = [script.name for script in scripts_dir.glob("*.py")]
 
     # Run each script in the analysis script directory
     for script in scripts_to_run:
