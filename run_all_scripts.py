3f7a110067694d6bf5e3b3ee5e320f9cbdc77731
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-10-14 16:34:57 +0200

    Refactor code to check path

--- run_all_scripts.py
+++ run_all_scripts.py
@@ -8,8 +8,12 @@ scripts_dir = Path(__file__).resolve().parent / variables.SCRIPTS_DIR
 
 # Function to run a script
 def run_script(script_name):
+    script_path = scripts_dir / script_name
+    if not script_path.exists():
+        print(f"Script {script_name} not found.")
+        return
     try:
-        result = subprocess.run(["python3", scripts_dir / script_name], check=True, capture_output=True, text=True)
+        result = subprocess.run(["python3", script_path], check=True, capture_output=True, text=True)
         print(f"Successfully ran {script_name}:\n{result.stdout}{'=' * 40}\n")
     except subprocess.CalledProcessError as e:
         print(f"There was an error running {script_name}:\n{e.stderr}{'=' * 40}\n")
