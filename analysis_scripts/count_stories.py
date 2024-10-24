be946321d8c5b49d787626e89ebe031fba8f360a
Thea Holtlund Jacobsen <117310317+theaholtlund@users.noreply.github.com>
2024-10-24 10:30:40 +0200

    Add output confirmation

--- analysis_scripts/count_stories.py
+++ analysis_scripts/count_stories.py
@@ -12,6 +12,7 @@ import variables
 from utils import get_script_dir, construct_file_path, write_to_file
 
 def main():
+    """Count the total number of files in the stories directory and save to a text file."""
     script_dir = get_script_dir()
     stories_path = construct_file_path(script_dir, variables.DATA_DIR, variables.MEDIA_DIR, variables.STORIES_DIR)
     
@@ -21,6 +22,7 @@ def main():
     # Construct output file path and output result to file
     output_file_path = construct_file_path(variables.OUTPUT_DIR, "count_stories.txt")
     write_to_file(output_file_path, [total_files], "Total number of files in stories: ", detailed=False)
+    print(f"Total number of files in stories has been saved to '{output_file_path}'.")
 
 if __name__ == "__main__":
     main()
