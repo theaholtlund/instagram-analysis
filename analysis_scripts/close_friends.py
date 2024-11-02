6154f5bec59feb3b220482535349161e2f95cd6d
Thea Holtlund Jacobsen <117310317+theaholtlund@users.noreply.github.com>
2024-11-02 08:14:46 +0100

    Refactor usernames and confirmation

--- analysis_scripts/close_friends.py
+++ analysis_scripts/close_friends.py
@@ -18,11 +18,16 @@ def main():
     # Load, parse and extract close friends accounts from HTML content
     close_friends_path = construct_file_path(script_dir, variables.DATA_DIR, variables.CONNECTIONS_DIR, variables.FOLLOWERS_DIR, variables.CLOSE_FRIENDS_FILE)
     close_friends_soup = load_and_parse_html(close_friends_path)
-    close_friends_usernames = extract_usernames(close_friends_soup)
-    
+
+    # Extract URLs and then convert to usernames
+    close_friends_usernames = extract_content(close_friends_soup, tag="a", content_type="href")
+
+    # Extract only the username from each URL
+    usernames_only = {url.split('/')[-1] for url in close_friends_usernames}
+
     # Construct output file path and output result to file
     output_file_path = construct_file_path(variables.OUTPUT_DIR, "close_friends.txt")
-    write_to_file(output_file_path, close_friends_usernames, "Number of close friends", detailed=True, data_label="Users on close friends list")
+    write_to_file(output_file_path, usernames_only, "Number of close friends", detailed=True, data_label="Users on close friends list")
     print(f"Close friends list has been saved to '{output_file_path}'.")
 
 if __name__ == "__main__":
