fe3262007d1f57fbb60e2e1714ef00816df3cd21
Thea Holtlund Jacobsen <117310317+theaholtlund@users.noreply.github.com>
2024-11-05 07:26:20 +0100

    Add initial outline for function to create plot

--- summary_report/create_plot.py
+++ summary_report/create_plot.py
@@ -1,3 +1,15 @@
 # Import required libraries
 import matplotlib.pyplot as plt
 from pathlib import Path
+
+def create_activity_plot(comments_file, likes_file):
+    comments_path = Path(comments_file)
+    likes_path = Path(likes_file)
+
+    with comments_path.open("r") as f:
+        comments_line = f.readline().strip()
+        total_comments = int(comments_line.split(":")[1].strip())
+
+    with likes_path.open("r") as f:
+        likes_line = f.readline().strip()
+        total_likes = int(likes_line.split(":")[1].strip())
