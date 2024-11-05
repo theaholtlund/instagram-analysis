da44bbe32f59dcb457c8412bb59d9cdab57b27cd
Thea Holtlund Jacobsen <117310317+theaholtlund@users.noreply.github.com>
2024-11-05 07:28:03 +0100

    Add error handling to function

--- summary_report/create_plot.py
+++ summary_report/create_plot.py
@@ -6,10 +6,13 @@ def create_activity_plot(comments_file, likes_file):
     comments_path = Path(comments_file)
     likes_path = Path(likes_file)
 
-    with comments_path.open("r") as f:
-        comments_line = f.readline().strip()
-        total_comments = int(comments_line.split(":")[1].strip())
-
-    with likes_path.open("r") as f:
-        likes_line = f.readline().strip()
-        total_likes = int(likes_line.split(":")[1].strip())
+    try:
+        with comments_path.open("r") as f:
+            comments_line = f.readline().strip()
+            total_comments = int(comments_line.split(":")[1].strip())
+        
+        with likes_path.open("r") as f:
+            likes_line = f.readline().strip()
+            total_likes = int(likes_line.split(":")[1].strip())
+    except:
+        print("Error reading data files")
