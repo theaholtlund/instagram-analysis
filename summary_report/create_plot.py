4efb8d2d4acdc1187ffcb87ee23da305c001af93
Thea Holtlund Jacobsen <117310317+theaholtlund@users.noreply.github.com>
2024-12-08 13:00:07 +0100

    Add details to report plot

--- summary_report/create_plot.py
+++ summary_report/create_plot.py
@@ -117,3 +117,6 @@ def create_close_friends_ratio_plot(output_path, close_friends_file, following_f
         color=['green', 'gray']
     )
     plt.title(f"Following vs Close Friends\nClose Friends: {num_close_friends} | Following: {len(following_usernames)}\nPercentage: {percentage:.2f}%")
+    plt.xlabel("Category")
+    plt.ylabel("Count")
+    plt.tight_layout()
