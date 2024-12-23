8679ed477a3dae76cb150d195d3b8dc8bc87347d
Thea Holtlund Jacobsen <117310317+theaholtlund@users.noreply.github.com>
2024-12-23 08:13:27 +0100

    Update names for chart files

--- variables.py
+++ variables.py
@@ -23,8 +23,8 @@ REPORT_TEMPLATE = "report_template.html"
 SUMMARY_REPORT = "summary_report.html"
 
 # Constants for styling of report plots
-PLOT_BAR_CHART = "activity_chart_bar.png"
-PLOT_PIE_CHART = "activity_chart_pie.png"
-PLOT_FOLLOWING_CHART = "following_vs_close_friends.png"
+PLOT_BAR_CHART = "chart_bar_activity.png"
+PLOT_PIE_CHART = "chart_pie_activity.png"
+PLOT_FOLLOWING_CHART = "chart_close_friends_ratio.png"
 PLOT_COLOUR_LIGHT = "#FFA64E"
 PLOT_COLOUR_DARK = "#FF8B19"
